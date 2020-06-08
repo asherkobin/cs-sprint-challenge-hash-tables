63d6c80cf56332c9172c00e85a2f77f37c02803e
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-06-07 20:15:44 -0400

    Weights + Duplicates

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,21 +9,32 @@ def get_indices_of_item_weights(weights, _, limit):
     """
     weight_table = {}
 
+    # build table of k=weight v=[idxs]
     for idx, weight in enumerate(weights):
-      weight_table[weight] = [limit - weight, idx]
+      # because there may be items of the same weight, the index is stored in an array
+      if weight not in weight_table:
+        weight_table[weight] = [idx] # first
+      else:
+        weight_table[weight].append(idx) # additional (only need two of them via rules)
 
-    for weight in weight_table:
-      needed_weight = weight_table[weight][0]
+    # loop again trying to locate the matching weight
+    for weight in weights:
+      needed_weight = limit - weight
+    
+      # special case for items with the same weight
+      if needed_weight == weight and len(weight_table[weight]) > 1:
+        idx1 = weight_table[weight][1] # this idx is always larger
+        idx2 = weight_table[weight][0]
+        return (idx1, idx2)
+      
       if needed_weight in weight_table:
         if weight + needed_weight == limit:
-          idx1 = weight_table[weight][1]
-          idx2 = weight_table[needed_weight][1]
-          if idx1 > idx2:
+          # found the 2nd weight
+          idx1 = weight_table[weight][0]
+          idx2 = weight_table[needed_weight][0]
+          if idx1 > idx2: # don't understand the purpose of this rule but ok
             return (idx1, idx2)
           else:
             return (idx2, idx1)
 
     return None
-
-print(get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 0, 21))
-print(get_indices_of_item_weights([ 4, 4 ], 0, 8))
